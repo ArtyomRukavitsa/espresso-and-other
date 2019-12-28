4afcc5b9aaf697c5319943dd81ea850e818d6c18
ArtyomRukavitsa <rukavitsa.a@yandex.ru>
2019-12-28 11:17:24 +0300

    Задача Капучино (проблемы с окном)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow,QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow,QTableWidgetItem, QWidget
 from PyQt5 import uic
 import sqlite3
 
-
-class MyWidget(QMainWindow):
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
@@ -14,6 +13,7 @@ class MyWidget(QMainWindow):
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setHorizontalHeaderLabels(['id', 'название сорта', 'степень обжарки',
                                                     'молотый/зерна', 'вкус', 'цена', 'объем упаковки'])
+        self.pushButton_2.clicked.connect(self.newForm)
 
     def select(self):
         req = f"SELECT * FROM Cof WHERE id = {int(self.spinBox.text())}"
@@ -21,6 +21,25 @@ class MyWidget(QMainWindow):
         result = cur.execute(req).fetchone()
         for i in range(len(result)):
             self.tableWidget.setItem(0, i, QTableWidgetItem(str(result[i])))
+
+    def newForm(self):
+        NewWidget()
+
+
+
+class NewWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.show()
+        #self.con = sqlite3.connect("coffee.sqlite3")
+        self.show()
+        #self.pushButton.clicked.connect(self.select)
+        #self.tableWidget.setRowCount(1)
+        #self.tableWidget.setColumnCount(7)
+        #self.tableWidget.setHorizontalHeaderLabels(['id', 'название сорта', 'степень обжарки',
+         #                                           'молотый/зерна', 'вкус', 'цена', 'объем упаковки'])
+        #self.pushButton_2.clicked.connect(self.newForm)
 
 
 app = QApplication(sys.argv)
